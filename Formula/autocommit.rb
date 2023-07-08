23b59172fb19b15e6521d341e5ea442c5420a0cb
Raghav <dev@raghav98.com>
2023-07-08 12:30:28 -0400

    release version 1.4

--- Formula/autocommit.rb
+++ Formula/autocommit.rb
@@ -4,9 +4,9 @@
 class Autocommit < Formula
   desc "A simple tool to automatically commit changes using LLM generated commit summaries."
   homepage "https://github.com/raghavanand98/autocommit"
-  url "https://github.com/raghavanand98/autocommit/releases/download/v0.1.3/autocommit-x86_64-apple-darwin.tar.xz"
-  sha256 "1ef3a8fe3bd58ce6b17b832ded0ad47a684ba7f09b189ec2b0eba0f83df00525"
-  version "0.1.3"
+  url "https://github.com/raghavanand98/autocommit/releases/download/v0.1.4/autocommit-x86_64-apple-darwin.tar.xz"
+  sha256 "d2dbe30209b49d6fa0b4cb14e02d7f738def974bd5ae1367c6b0f987a4b2c508"
+  version "0.1.4"
 
   def install
     bin.install "autocommit"
